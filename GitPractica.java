0974f256528d4c6aabf2060d95e0bbbef8204cc6
mauocampo7 <mauricio1522ocampo@gmail.com>
2024-02-28 21:32:58 -0600

    mi segundo cambio

--- GitPractica.java
+++ GitPractica.java
@@ -3,8 +3,8 @@ package Git;
 public class GitPractica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+      System.out.println("Familia tenemos que hablar ");
+      
 	}
-
 }
